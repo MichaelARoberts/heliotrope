f575ebddd79f304c9a756d487b40452a786ddaee
Cristian Medeiros <cristianmedeiros@gmail.com>
2016-06-22 09:05:31 -0300

    Adding Display trait

--- src/document.rs
+++ src/document.rs
@@ -69,14 +69,14 @@ impl fmt::Display for SolrValue {
         // stream: `f`. Returns `fmt::Result` which indicates whether the
         // operation succeeded or failed. Note that `write!` uses syntax which
         // is very similar to `println!`.
-        // let printable = match *self {
-        //     SolrValue::I64(i64) => &self,
-        //     SolrValue::U64(u64) => &self,
-        //     SolrValue::F64(f64) => &self,
-        //     SolrValue::String(String) => &self,
-        //     SolrValue::Boolean(bool) => &self,
-        //     SolrValue::Null => &self
-        // };
-        write!(f, "{}", self)
+        match *self {
+            SolrValue::I64(ref val) => write!(f, "{}", val),
+            SolrValue::U64(ref val) => write!(f, "{}", val),
+            SolrValue::F64(ref val) => write!(f, "{}", val),
+            SolrValue::String(ref val) => write!(f, "{}", val),
+            SolrValue::Boolean(ref val) => write!(f, "{}", val),
+            SolrValue::Null => write!(f, "{}", "NULL")
+        };
+        
     }
 }
