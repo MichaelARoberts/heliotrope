e94094241ab2e00db47e58506137eed6150f7c51
Cristian Medeiros <cristianmedeiros@gmail.com>
2016-06-21 22:42:36 -0300

    Adding Display trait

--- src/document.rs
+++ src/document.rs
@@ -60,3 +60,14 @@ impl Encodable for SolrDocument {
         })
     }
 }
+
+impl fmt::Display for SolrField {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}", self.1)
+    }
+}
